35758804fc26a6127baefd538e7c68d3f3808569
Berkayuguralpsahin <65742494+Berkayuguralpsahin@users.noreply.github.com>
2022-06-24 17:18:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BtkAkademi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int A=20, B=10;
+           /*
+            Console.WriteLine(A+ B);
+            Console.WriteLine(A- B);
+            Console.WriteLine(A * B);
+            Console.WriteLine(A / B);
+            Console.WriteLine(A % B);
+            */
+            // İLİŞKİSEL
+            /*
+            Console.WriteLine(A>B);
+            Console.WriteLine(A < B);
+            Console.WriteLine(A >= B);
+            Console.WriteLine(A <= B);
+            Console.WriteLine(A == B);
+            Console.WriteLine(A != B);
+            */
+            Console.WriteLine(A>B && A<5);
+            Console.WriteLine(A < B || B>5);
+            Console.ReadKey();
+          
+        }
+    }
+}
